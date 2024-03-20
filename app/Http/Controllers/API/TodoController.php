73482828f87de794db8ef7a0338b42d168adc798
Lewe Maiga <badraliyoumaiga199@gmail.com>
2024-03-20 02:24:39 +0000

    first commit

---
+++ app/Http/Controllers/API/TodoController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $todos = Todo::all();
+        return response()->json($todos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $todos = Todo::create($request->all());
+        return response()->json($todos);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        $todo = Todo::find($todo->id);
+        return response()->json($todo);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $todo->update($request->all());
+        return response()->json($todo);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return response()->json(null,0);
+    }
+}
